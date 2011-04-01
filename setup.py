55938ac9a0d738257776ab69488570fc4efead1a
Johannes Gorset <jgorset@gmail.com>
2011-04-01 14:03:03 +0200

    Bump version to 3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = 'fandjango',
-  version = '3.1.1',
+  version = '3.2',
   description = "Fandjango is a library for Facebook canvas applications powered by Django",
   author = "Johannes Gorset",
   author_email = "jgorset@gmail.com",
