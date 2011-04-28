9814b2c175a3a0d431891c2aea589a00dcca3c23
Johannes <jgorset@gmail.com>
2011-04-28 20:49:43 +0200

    Fix regular expression

--- fandjango/urls.py
+++ fandjango/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    url(r'authorize_application.html$', authorize_application, name='authorize_application')
+    url(r'^authorize_application.html$', authorize_application, name='authorize_application')
 )
