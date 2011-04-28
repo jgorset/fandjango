29e39f485661670a7110df5d5c6681be6b726b3d
Johannes <jgorset@gmail.com>
2011-04-28 20:49:09 +0200

    Add docstring

--- fandjango/views.py
+++ fandjango/views.py
@@ -2,6 +2,10 @@ from utils import redirect_to_facebook_authorization
 from settings import FACEBOOK_APPLICATION_URL
 
 def authorize_application(request):
+    """
+    Redirect to Facebook authorization, then redirect back to the value of the 'redirect_uri' query string
+    or default to FACEBOOK_APPLICATION_URL.
+    """
     return redirect_to_facebook_authorization(
         redirect_uri = request.GET['redirect_uri'] if request.GET.has_key('redirect_uri') else FACEBOOK_APPLICATION_URL
     )
