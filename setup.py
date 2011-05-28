a254b8ad75b23abdafd5c84fd69048f0750c47b9
Johannes <jgorset@gmail.com>
2011-05-28 23:36:18 +0200

    Fix indentation

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from distutils.core import setup
 import fandjango
 
 setup(
-  name = 'fandjango',
-  version = fandjango.__version__,
-  description = "Fandjango makes it easy to create Facebook applications powered by Django",
-  author = "Johannes Gorset",
-  author_email = "jgorset@gmail.com",
-  url = "http://github.com/jgorset/fandjango",
-  packages = ['fandjango', 'fandjango.migrations', 'fandjango.templatetags'],
-  package_data = {
-    'fandjango': ['templates/*']
-  }
+    name = 'fandjango',
+    version = fandjango.__version__,
+    description = "Fandjango makes it easy to create Facebook applications powered by Django",
+    author = "Johannes Gorset",
+    author_email = "jgorset@gmail.com",
+    url = "http://github.com/jgorset/fandjango",
+    packages = ['fandjango', 'fandjango.migrations', 'fandjango.templatetags'],
+    package_data = {
+        'fandjango': ['templates/*']
+    }
 )
