9b4c6f7ac0d4455f11fa8ac323c42f607c8b924c
Johannes Gorset <jgorset@gmail.com>
2011-08-13 00:38:31 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -14,5 +14,17 @@ setup(
     packages = ['fandjango', 'fandjango.migrations', 'fandjango.templatetags'],
     package_data = {
         'fandjango': ['templates/*']
-    }
+    },
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
