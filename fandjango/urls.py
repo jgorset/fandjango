f405ee262ab6f87df8e83e024d3c615842fa37ce
Johannes Gorset <jgorset@gmail.com>
2011-12-03 10:26:45 +0100

    Add a name to the URL configuration for 'deauthorize_application'

--- fandjango/urls.py
+++ fandjango/urls.py
@@ -4,5 +4,5 @@ from views import *
 
 urlpatterns = patterns('',
     url(r'^authorize_application.html$', authorize_application, name='authorize_application'),
-    url(r'^deauthorize_application.html$', deauthorize_application)
+    url(r'^deauthorize_application.html$', deauthorize_application, name='deauthorize_application')
 )
