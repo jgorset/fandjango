7013ba68bbdbd537dd25b6c338a469db62f9e4b4
Johannes Gorset <jgorset@gmail.com>
2011-12-03 10:41:33 +0100

    Include fandjango's URLs in tests

--- tests/app/urls.py
+++ tests/app/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls.defaults import *
 from views import home
 
 urlpatterns = patterns('',
-    url(r'^$', home, name='home')
+    url(r'^$', home, name='home'),
+    
+    url('fandjango/', include('fandjango.urls'))
 )
