ef64490bc06bff8b0da4b1aa84e470f398aa88a1
Johannes Gorset <jgorset@gmail.com>
2011-12-27 01:26:35 +0100

    Add trove classifiers

--- setup.py
+++ setup.py
@@ -15,5 +15,17 @@ setup(
     package_data = {
         'fandjango': ['templates/*']
     },
-    install_requires = ['facepy>=0.4.2', 'requests==0.7.6']
+    install_requires = [
+        'facepy>=0.4.2',
+        'requests==0.7.6'
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
 )
