690f763b8e99b42d8da4db2f2a0665f78cf6c4d7
Adem Gaygusuz <adem@ardweb.co.uk>
2013-08-19 00:29:27 +0100

    Added mocks to tests to simulate graph calls. Upped to Django 1.5.2 to resolve JSONField bug.

--- tests/project/settings.py
+++ tests/project/settings.py
@@ -1,6 +1,6 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:'
     }
 }
@@ -23,3 +23,7 @@ FACEBOOK_APPLICATION_ID = 181259711925270
 FACEBOOK_APPLICATION_SECRET_KEY = '214e4cb484c28c35f18a70a3d735999b'
 FACEBOOK_APPLICATION_NAMESPACE = 'fandjango-test'
 FACEBOOK_APPLICATION_CANVAS_URL = 'http://example.org/foo'
+
+SECRET_KEY = '123'
+
+USE_TZ = True
